60c05c23a5f61be612f871375b01cfadc46e9f40
Najil Arfak <najilarfak202@gmail.com>
2025-01-25 10:56:22 +0530

    url fix

--- habit_tracker/urls.py
+++ habit_tracker/urls.py
@@ -20,7 +20,7 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('habits/', include('habits.urls')),
+    path('', include('habits.urls')),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
